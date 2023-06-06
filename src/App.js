1708c0d55e20b825398c42e69b2afae4259b8ef0
Mitra-rk <119427404+Mitra-rk@users.noreply.github.com>
2023-06-06 13:06:16 +0330

    added github link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ export default function App() {
         <h1>What word do you want to look up?</h1>
         <Dictionary defaultWord="sunset" />
       </main>
-      <footer>Coded by Mitra Enayatollahi</footer>
+      <footer>
+        Coded by Mitra Enayatollahi and it is{" "}
+        <a href="https://github.com/Mitra-rk/my_dictionary2">
+          Open-sourced on GitHUb
+        </a>
+      </footer>
     </div>
   );
 }
